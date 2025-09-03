187c8bd85f8603bf3e93b9a7f97af0d3a965e92c
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2025-09-02 23:37:13 -0600

    Actualizar la estructura de renderizado en main.tsx: se elimina StrictMode y ThemeProvider, y se incorpora el nuevo componente Provider para gestionar el contexto de la aplicación.

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,14 @@
 import './index.css'
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Provider from './providers.tsx'
 import App from './App.tsx'
-import { ThemeProvider } from './contexts/theme-provider'
-
 import '@fontsource/be-vietnam-pro/400.css'
 import '@fontsource/be-vietnam-pro/500.css'
 import '@fontsource/be-vietnam-pro/600.css'
 import '@fontsource/be-vietnam-pro/700.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
+  <Provider>
+    <App />
+  </Provider>
 )
