799015df5cbb4d45e34ee79998513769f43f90e0
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2025-09-02 23:37:27 -0600

    Actualizar el componente App: se reemplaza SearchingContainer por Hero y se incorpora la lógica para mostrar información del usuario, incluyendo HeaderUser, InfoUser y RepositoryContainer. Se añade funcionalidad para visualizar todos los repositorios si el usuario tiene cuatro o más.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import './App.css'
-import SearchingContainer from './components/searching-container'
+import Hero from './components/hero'
+import HeaderUser from './components/header-user'
+import useUser from './hooks/useUser'
+import InfoUser from './components/info-user'
+import RepositoryContainer from './components/repository-container'
 
 function App() {
+  const { user, repositories, getAllRepositories } = useUser()
+
   return (
     <>
-      <SearchingContainer />
+      <Hero />
 
-      <div>
-        <p>app</p>
-      </div>
+      <main className="max-w-5xl mx-5 md:mx-14 xl:max-w-6xl xl:mx-auto xl:container pb-10 z-10 flex flex-col gap-8">
+        {user && (
+          <>
+            <HeaderUser {...user} />
+            <InfoUser name={user?.name} bio={user?.bio} />
+            <RepositoryContainer />
+
+            {repositories.length === 4 && (
+              <div className="flex justify-center mt-3">
+                <span
+                  className="text-foreground dark:text-mischka font-normal hover:text-oxford-blue dark:hover:text-white transition-colors cursor-pointer"
+                  onClick={getAllRepositories}
+                >
+                  View all repositories
+                </span>
+              </div>
+            )}
+          </>
+        )}
+      </main>
     </>
   )
 }
